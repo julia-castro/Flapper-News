52195e5fd98baf03dfa34e07383046981b1c66fa
julia-castro <julia.castro@outlook.com>
2015-09-10 19:59:37 -0400

    update template urls

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,12 +6,12 @@ function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: '/home',
-      templateUrl: '/home.html',
+      templateUrl: 'home/_home.html',
       controller: 'MainCtrl'
     });
     .state('posts', {
       url:'/posts/{id}',
-      templateUrl: '/posts.html',
+      templateUrl: 'posts/_posts.html',
       controller: 'PostsCtrl'
     });
   $urlRouterProvider.otherwise('home');
