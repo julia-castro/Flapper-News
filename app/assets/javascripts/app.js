5e3749eee74decfa73ae078c13ce0cf618e6d1b6
julia-castro <julia.castro@outlook.com>
2015-09-10 20:05:45 -0400

    Move posts service from app.js to posts/posts.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -16,18 +16,6 @@ function($stateProvider, $urlRouterProvider) {
     });
   $urlRouterProvider.otherwise('home');
 }])
-.factory('posts', [function(){
-  var o = {
-    posts: [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
-    ]
-  };
-  return o;
-}])
 .controller('MainCtrl', [
 '$scope','posts',
 function($scope, posts){
