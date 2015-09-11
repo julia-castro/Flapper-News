82d528d52392c854aa5fff08f1d2b86956d9ed8a
julia-castro <julia.castro@outlook.com>
2015-09-10 21:08:47 -0400

    inject http service and getall function

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -1,13 +1,19 @@
 angular.module('flapperNews')
-.factory('posts', [function(){
+.factory('posts', ['$http',
+  function($http){
   var o = {
     posts: [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
+  // {title: 'post 1', upvotes: 5},
+  // {title: 'post 2', upvotes: 2},
+  // {title: 'post 3', upvotes: 15},
+  // {title: 'post 4', upvotes: 9},
+  // {title: 'post 5', upvotes: 4}
     ]
   };
+  o.getAll = function() {
+    return $http.get('/posts.json').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
   return o;
-}])
+}]);
