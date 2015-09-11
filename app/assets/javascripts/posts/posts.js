202933ba38faa2717e0a8cf3cb71be44ca32f964
julia-castro <julia.castro@outlook.com>
2015-09-10 22:42:02 -0400

    add create method to posts service

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -2,17 +2,16 @@ angular.module('flapperNews')
 .factory('posts', ['$http',
   function($http){
   var o = {
-    posts: [
-  // {title: 'post 1', upvotes: 5},
-  // {title: 'post 2', upvotes: 2},
-  // {title: 'post 3', upvotes: 15},
-  // {title: 'post 4', upvotes: 9},
-  // {title: 'post 5', upvotes: 4}
-    ]
+    posts: []
   };
   o.getAll = function() {
     return $http.get('/posts.json').success(function(data){
       angular.copy(data, o.posts);
+    });
+  };
+  o.create = function(post){
+    return $http.post('/posts.json', post).success(function(data){
+      o.posts.push(data);
     });
   };
   return o;
