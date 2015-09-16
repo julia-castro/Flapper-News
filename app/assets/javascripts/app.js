50e229cf7deacaab100254ef2524864222a0c9fb
julia-castro <julia.castro@outlook.com>
2015-09-16 13:20:34 -0400

    fix bug in module

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,19 +1,20 @@
 angular.module('flapperNews', ['ui.router', 'templates', 'Devise'])
 .config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: 'home/_home.html',
-      controller: 'MainCtrl',
-      resolve: {
-        postPromise: ['posts', function(posts){
-          return posts.getAll();
-        }]
-      }})
-    .state('posts', {
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'home/_home.html',
+        controller: 'MainCtrl',
+        resolve: {
+          postPromise: ['posts',
+            function(posts){
+              return posts.getAll();
+            }]
+      }}).state('posts', {
         url: '/posts/{id}',
         templateUrl: 'posts/_posts.html',
         controller: 'PostsCtrl',
@@ -23,25 +24,26 @@ function($stateProvider, $urlRouterProvider) {
           }]
         }
       })
-    .state('login', {
-      url: '/login',
-      templateUrl: 'auth/_login.html',
-      controller: 'AuthCtrl'
-      onEnter: ['$state', 'Auth', function($state, Auth) {
-        Auth.currentUser().then(function (){
-          $state.go('home');
-        })
-      }]
-    })
-    .state('register', {
-      url: '/register',
-      templateUrl: 'auth/_register.html',
-      controller: 'AuthCtrl'
-      onEnter: ['$state', 'Auth', function($state, Auth) {
-        Auth.currentUser().then(function (){
-          $state.go('home');
-        })
-      }]
-    });
-  $urlRouterProvider.otherwise('home');
-}])
+      .state('login', {
+        url: '/login',
+        templateUrl: 'auth/_login.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function (){
+            $state.go('home');
+          })
+        }]
+      })
+      .state('register', {
+        url: '/register',
+        templateUrl: 'auth/_register.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function (){
+            $state.go('home');
+          })
+        }]
+      });
+
+    $urlRouterProvider.otherwise('home');
+}]);
